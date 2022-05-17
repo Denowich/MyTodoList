c578b2f12119d04435a6ac02078f55452823be2f
Eugene263 <kupulenko29@gmail.com>
2022-05-17 15:04:46 +0300

    delete dont needed comment

--- src/App.js
+++ src/App.js
@@ -3,22 +3,7 @@ import "./App.css";
 import ToDoItemList from './ToDoItemList/ToDoItemList.js';
 
 class App extends Component {
-
-
-  // handleChange = id => {
-  //   const index = this.state.todoItems.map(item => item.id).indexOf(id);
-  //   this.setState(state => {
-  //     let { todoItems } = state;
-  //     todoItems[index].completed = true;
-  //     return todoItems;
-  //   });
-  // };
-
   render() {
-    // const { todoItems } = this.state;
-    // const activeTasks = todoItems.filter(task => task.completed === false);
-    // const completedTasks = todoItems.filter(task => task.completed === true);
-    // const finalTasks = [...activeTasks, ...completedTasks].map(item => {
     return (
       <>
         <div className="App">
